7923039bfcb408034e9d7f06db95a9913727fe27
AnnihilatoR <AnnihilatoR@localhost>
2026-02-25 13:02:18 +0300

    temporarily removed

---
+++ MemoryCells/_BaseFormUnit.pas
@@ -0,0 +1,31 @@
+﻿unit BaseFormUnit;
+
+interface
+
+uses
+    System.Classes
+  , System.UITypes
+  , FMX.FormExtUnit
+  , ThreadRegistryUnit
+  ;
+
+type
+  TBaseForm = class(TFormExt)
+  strict private
+  private
+  public
+    procedure Repaint;
+  end;
+
+implementation
+
+uses
+    System.SysUtils
+  ;
+
+procedure TBaseForm.Repaint;
+begin
+  Self.PaintRects([Self.ClientRect]);
+end;
+
+end.
